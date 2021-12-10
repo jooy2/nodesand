86be9e3d59e4ae98cac10fa202f1eac647938e8d
leejooy96 <leejooy96@gmail.com>
2021-12-10 13:18:26 +0900

    fix eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    'env': {
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'airbnb-base'
+    ],
+    'parserOptions': {
+        'ecmaVersion': 12
+    },
+    'rules': {
+        'linebreak-style': [
+            'error', 'unix'
+        ],
+        'import/extensions': 0
+    }
+}
